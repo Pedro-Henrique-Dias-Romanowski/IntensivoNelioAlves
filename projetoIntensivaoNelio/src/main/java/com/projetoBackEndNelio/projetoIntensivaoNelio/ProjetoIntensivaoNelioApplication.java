33a12d23b50a54cf513106e168142bfdfcaad9ad
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-20 16:24:00 -0300

    primeiro commit

---
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/ProjetoIntensivaoNelioApplication.java
@@ -0,0 +1,13 @@
+package com.projetoBackEndNelio.projetoIntensivaoNelio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntensivaoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntensivaoNelioApplication.class, args);
+	}
+
+}
---
+++ projetoIntensivaoNelio/src/test/java/com/projetoBackEndNelio/projetoIntensivaoNelio/ProjetoIntensivaoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoBackEndNelio.projetoIntensivaoNelio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntensivaoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
