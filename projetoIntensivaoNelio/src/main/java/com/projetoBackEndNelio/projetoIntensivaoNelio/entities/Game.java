c527e07a1388380791d25fca59d4c1d89a46c092
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-20 20:43:13 -0300

    fix: alÃterando o nome da coluna que estava errada

--- projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/entities/Game.java
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/entities/Game.java
@@ -26,14 +26,16 @@ public class Game {
 
     private String genre;
 
-    private String plataform;
+    private String platforms;
 
     private Double score;
 
     private String imgUrl;
 
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
 
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
 }
