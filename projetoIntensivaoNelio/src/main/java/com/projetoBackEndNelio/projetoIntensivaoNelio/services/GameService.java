5f6f6813c3b3b6cef1a30c24b9ea7e5ca97aa5d0
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-21 20:34:29 -0300

    feat: adicionando o método findById na camada de serviço

--- projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/services/GameService.java
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/services/GameService.java
@@ -1,11 +1,15 @@
 package com.projetoBackEndNelio.projetoIntensivaoNelio.services;
 
 
+import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameDTO;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameMinDTO;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.entities.Game;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.repositories.GameRepository;
+import com.projetoBackEndNelio.projetoIntensivaoNelio.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -15,12 +19,21 @@ public class GameService {
     private GameRepository gameRepository;
 
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> listGames = gameRepository.findAll();
         return listGames.stream().map(x -> new GameMinDTO(x.getId(), x.getTitle(), x.getYear(), x.getImgUrl(),
                         x.getShortDescription()))
                         .toList();
     }
+
+    @Transactional(readOnly = true)
+   public GameDTO findById(Long id){
+        Game game = gameRepository.findById(id).orElseThrow(()
+                -> new ResourceNotFoundException("Game not found"));
+
+       return new GameDTO(game);
+   }
 
 
 }
