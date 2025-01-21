26a7df8729c2970d71521a3483125d484792667d
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-21 15:47:21 -0300

    feat: criando a classe Game List, que vai representar uma lista de games

---
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/entities/GameList.java
@@ -0,0 +1,21 @@
+package com.projetoBackEndNelio.projetoIntensivaoNelio.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
