ea740990bd5430114b90b3c74af4d67ba4e165a7
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-21 14:43:00 -0300

    delete: removendo o construtor que recebia o dto como parâmetro, n precisa mais dele

--- projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/entities/Game.java
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/entities/Game.java
@@ -12,24 +12,15 @@ import lombok.*;
 @Entity
 @Table(name = "tb_game")
 public class Game {
-
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
-
-
     @Column(name = "game_year")
     private Integer year;
-
     private String genre;
-
     private String platforms;
-
     private Double score;
-
     private String imgUrl;
 
     @Column(columnDefinition = "TEXT")
@@ -37,5 +28,4 @@ public class Game {
 
     @Column(columnDefinition = "TEXT")
     private String longDescription;
-
 }
