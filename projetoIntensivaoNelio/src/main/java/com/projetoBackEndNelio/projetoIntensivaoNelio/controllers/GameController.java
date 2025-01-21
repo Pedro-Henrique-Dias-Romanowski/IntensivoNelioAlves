23542906b8199d20eee7e797e4527d38e68f5321
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-21 20:32:50 -0300

    feat: adicionando o método findById

--- projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/controllers/GameController.java
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.projetoBackEndNelio.projetoIntensivaoNelio.controllers;
 
 
+import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameDTO;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameMinDTO;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class GameController {
     public List<GameMinDTO> findAll(){
        return service.findAll();
    }
+
+   @GetMapping("/{id}")
+    public GameDTO findById(@PathVariable Long id){
+       return service.findById(id);
+    }
 
 }
