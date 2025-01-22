d93d492f6942474dff3e2be16fba528e7564c4d4
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-22 19:41:03 -0300

    feat: adicionando um construtor que recebe a projection como parâmetro para ser serializada

--- projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/dto/GameMinDTO.java
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/dto/GameMinDTO.java
@@ -1,9 +1,17 @@
 package com.projetoBackEndNelio.projetoIntensivaoNelio.dto;
 
 import com.projetoBackEndNelio.projetoIntensivaoNelio.entities.Game;
+import com.projetoBackEndNelio.projetoIntensivaoNelio.projections.GameMinProjection;
 
 public record GameMinDTO(Long id, String title, Integer year, String imgUrl, String shortDescription) {
+
     public GameMinDTO(Game game){
         this(game.getId(), game.getTitle(), game.getYear(), game.getImgUrl(), game.getShortDescription());
+    }
+
+    public GameMinDTO(GameMinProjection gameMinProjection){
+        this(gameMinProjection.getId(), gameMinProjection.getTitle(), gameMinProjection.getYear(),
+                gameMinProjection.getImgUrl(), gameMinProjection.getShortDescription());
+
     }
 }
