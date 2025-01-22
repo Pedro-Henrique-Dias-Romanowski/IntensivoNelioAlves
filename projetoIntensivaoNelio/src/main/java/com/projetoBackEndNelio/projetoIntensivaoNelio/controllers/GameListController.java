89c0a8b514e9743562ef739d25b5c5e3982ece6f
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-22 19:38:15 -0300

    feat: adicionando o método de findByÃList

--- projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/controllers/GameListController.java
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/controllers/GameListController.java
@@ -2,9 +2,12 @@ package com.projetoBackEndNelio.projetoIntensivaoNelio.controllers;
 
 
 import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameListDTO;
+import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameMinDTO;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.services.GameListService;
+import com.projetoBackEndNelio.projetoIntensivaoNelio.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +20,17 @@ public class GameListController {
     @Autowired
     GameListService gameListService;
 
+    @Autowired
+    GameService gameService;
+
 
     @GetMapping("/findAll")
     public List<GameListDTO> findAll(){
         return gameListService.findAll();
+    }
+
+    @GetMapping("/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId){
+        return gameService.findByList(listId);
     }
 }
