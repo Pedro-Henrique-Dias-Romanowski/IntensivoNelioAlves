1a2ac2cfe483776e1ee335d8842fd53c99a8d28e
Pedro Henrique <pedro.romanowski02@gmail.com>
2025-01-25 17:10:36 -0300

    feat: adicionando o mÂétodo move na controladora

--- projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/controllers/GameListController.java
+++ projetoIntensivaoNelio/src/main/java/com/projetoBackEndNelio/projetoIntensivaoNelio/controllers/GameListController.java
@@ -3,13 +3,11 @@ package com.projetoBackEndNelio.projetoIntensivaoNelio.controllers;
 
 import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameListDTO;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.GameMinDTO;
+import com.projetoBackEndNelio.projetoIntensivaoNelio.dto.ReplacementDTO;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.services.GameListService;
 import com.projetoBackEndNelio.projetoIntensivaoNelio.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,10 @@ public class GameListController {
     @GetMapping("/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable Long listId){
         return gameService.findByList(listId);
+    }
+
+    @PostMapping("/{listId}/replacement")
+    public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+        gameListService.move(listId, body.sourceIndex(), body.destinationIndex());
     }
 }
